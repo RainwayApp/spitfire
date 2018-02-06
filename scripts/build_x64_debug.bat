2c32b2ef3e89604816ca801c3073ec8e01781535
Naomi Ahmed <yukimono6@gmail.com>
2018-02-06 18:37:09 +0000

    [lib] generate pc_test_mock

--- scripts/build_x64_debug.bat
+++ scripts/build_x64_debug.bat
@@ -2,4 +2,5 @@
 cd webrtc-checkout\src\
 call gn gen out/x64/Debug --args="is_debug=true target_cpu=\"x64\" symbol_level=2 rtc_enable_sctp=true fatal_linker_warnings=false treat_warnings_as_errors=false rtc_include_tests=true"
 call ninja -C out/x64/Debug
+call ninja -C out/x64/Debug pc_test_mock
 pause
--- scripts/build_x86_debug.bat
+++ scripts/build_x86_debug.bat
@@ -2,4 +2,5 @@
 cd webrtc-checkout\src\
 call gn gen out/x86/Debug --args="is_debug=true target_cpu=\"x86\" symbol_level=2 rtc_enable_sctp=true fatal_linker_warnings=false treat_warnings_as_errors=false rtc_include_tests=true"
 call ninja -C out/x86/Debug
+call ninja -C out/x86/Debug pc_test_mock
 pause
